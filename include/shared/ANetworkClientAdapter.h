1d5ce7b9e5b64a1c6ea74da1afc09785ee752a07
Romain-P <romain.pillot@epitech.eu>
2018-05-30 01:36:26 +0200

    created network client adapter

---
+++ include/shared/ANetworkClientAdapter.h
@@ -0,0 +1,47 @@
+//
+// Created by romain on 29/05/18.
+//
+
+#ifndef CPP_INDIE_STUDIO_INETWORKADAPTER_H
+#define CPP_INDIE_STUDIO_INETWORKADAPTER_H
+
+#include <sys/types.h>
+#include <shared/NetworkClient.h>
+#include <memory>
+#include "ANetworkBuffer.h"
+#include "INetworkClientHandler.h"
+
+using client_config_t = struct client_config_s;
+class NetworkClient;
+
+class ANetworkClientAdapter {
+public:
+
+    explicit ANetworkClientAdapter(size_t readSize) : _readSize(readSize), _handler() {}
+    virtual ~ANetworkClientAdapter() = default;
+
+    virtual void onDataReceived(NetworkClient *client, char *buffer, size_t length) = 0;
+    virtual std::unique_ptr<ANetworkBuffer> bufferFactory() = 0;
+
+    inline INetworkClientHandler *getHandler() {
+        if (_handler == nullptr)
+            _handler = std::move(defineClientHandler());
+
+        return _handler.get();
+    }
+
+    inline size_t readSize() const {
+        return _readSize;
+    }
+
+protected:
+
+    virtual std::unique_ptr<INetworkClientHandler> defineClientHandler() = 0;
+
+private:
+
+    size_t _readSize;
+    std::unique_ptr<INetworkClientHandler> _handler;
+};
+
+#endif //CPP_INDIE_STUDIO_INETWORKADAPTER_H
