a9f888c09f79246fa254f66b6f360671c6aa7110
Romain-P <romain.pillot@epitech.eu>
2018-05-29 12:57:43 +0200

    began network socket class

---
+++ sources/network/NetworkSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by romain on 29/05/18.
+//
+
+#include "NetworkSocket.h"
---
+++ sources/network/NetworkSocket.h
@@ -0,0 +1,17 @@
+//
+// Created by romain on 29/05/18.
+//
+
+#ifndef CPP_INDIE_STUDIO_NETWORKSOCKET_H
+#define CPP_INDIE_STUDIO_NETWORKSOCKET_H
+
+
+class NetworkSocket {
+public:
+
+private:
+
+};
+
+
+#endif //CPP_INDIE_STUDIO_NETWORKSOCKET_H
