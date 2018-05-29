28d941c22aecd2c92513bbda3e4e7399a5bc1811
Romain-P <romain.pillot@epitech.eu>
2018-05-30 01:36:09 +0200

    created network client handler interface

---
+++ include/shared/INetworkClientHandler.h
@@ -0,0 +1,25 @@
+//
+// Created by romain on 29/05/18.
+//
+
+#ifndef CPP_INDIE_STUDIO_NETWORKCLIENTHANDLER_H
+#define CPP_INDIE_STUDIO_NETWORKCLIENTHANDLER_H
+
+#include <sys/types.h>
+
+class NetworkClient;
+
+class INetworkClientHandler {
+public:
+
+    virtual ~INetworkClientHandler() = default;
+
+    virtual void onConnect(NetworkClient *client) = 0;
+    virtual void onReceive(NetworkClient *client, char *buffer, size_t length) = 0;
+    virtual void onSent(NetworkClient *client, char *buffer, size_t length) = 0;
+    virtual void onDisconnect(NetworkClient *client) = 0;
+
+};
+
+
+#endif //CPP_INDIE_STUDIO_NETWORKCLIENTHANDLER_H
