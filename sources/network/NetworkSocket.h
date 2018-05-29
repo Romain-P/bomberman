b2a460e92e7c905f05eb15136243f7abb28f9037
Romain-P <romain.pillot@epitech.eu>
2018-05-29 15:22:20 +0200

    added socket base functionality

--- sources/network/NetworkSocket.h
+++ sources/network/NetworkSocket.h
@@ -6,11 +6,53 @@
 #define CPP_INDIE_STUDIO_NETWORKSOCKET_H
 
 
+#include <netinet/in.h>
+#include "Network.h"
+
 class NetworkSocket {
 public:
 
-private:
+    enum Mode { ACTIVE, PASSIVE, CHILD };
 
+    /**
+     * Create a socket:
+     * Active: Connect() will be used to create the connection.
+     * Passive: Bind() and Listen() will be used to create a server socket.
+     *
+     * @param ip        ip to connect/listen
+     * @param port      port to connect
+     */
+    NetworkSocket(std::string &ip, uint16_t port, Mode mode) : _ip(ip), _port(port), _mode(mode) {};
+
+    /**
+     * Create a socket from a given server file descriptor
+     * Accept() will be used to get current socket id.
+     *
+     * @param server
+     */
+    explicit NetworkSocket(socket_fd_t server) : _server_fd(server), _mode(CHILD) {};
+
+    /**
+     * Initialise the socket
+     */
+    void init();
+
+    std::string const &getIpAddress() const;
+    uint16_t getPort() const;
+    Mode getMode() const;
+    bool isClosed() const;
+
+private:
+    std::string _ip;
+    uint16_t _port = 0;
+    socket_fd_t _server_fd = -1;
+    Mode _mode;
+
+    socket_fd_t _id;
+    bool _closed = false;
+
+    void create(bool listen);
+    void accept();
 };
 
 
