88bab7de8385462e2aa53233db9d34ddaf904303
Romain-P <romain.pillot@epitech.eu>
2018-05-30 01:37:23 +0200

    added network buffer missing

---
+++ sources/shared/ANetworkBuffer.cpp
@@ -0,0 +1,17 @@
+//
+// Created by romain on 29/05/18.
+//
+
+#include "shared/ANetworkBuffer.h"
+
+ssize_t ANetworkBuffer::getPosition() const {
+    return _position;
+}
+
+void ANetworkBuffer::setPosition(size_t position) {
+    _position = position;
+}
+
+size_t ANetworkBuffer::getMaxSize() const {
+    return _maxSize;
+}
