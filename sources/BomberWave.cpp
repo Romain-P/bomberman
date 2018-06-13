60d5e6af248ca75166519e855798fad2c1d693a5
Antonin Rapini <Mardel@pc-13.home>
2018-06-13 12:29:56 +0200

    added multiplayer join

--- sources/BomberWave.cpp
+++ sources/BomberWave.cpp
@@ -69,7 +69,8 @@ void BomberWave::LaunchSolo()
 void BomberWave::LaunchMultiplayerHost()
 {
     Device->getGUIEnvironment()->clear();
-    GameServer server("127.0.0.1", RANDOM_PORT);
+    GameServer server("127.0.0.1", 4242);
+    //GameServer server("127.0.0.1", RANDOM_PORT);
     std::thread thread([&server] { server.start() ;});
 
     while (server.getPort() == 0);
@@ -105,8 +106,31 @@ void BomberWave::LaunchMultiplayerJoin()
 {
     Device->getGUIEnvironment()->clear();
     GameSessionConnector connector;
-    uint16_t port = 42;
+    uint16_t port = 4242;
     std::thread con([&connector, &port] { connector.tryConnect("127.0.0.1", port); });
+    while (!connector.isClosed() && Device->run())
+    {
+        switch(connector.getState())
+        {
+            case GameSessionController::State::ON_LOBBY:
+            {
+                connector.getController().getlobby()->Run();
+                break;
+            }
+            case GameSessionController::State::ON_GAME:
+            {
+                if (!connector.getController().getGameManager()->Update())
+                {
+                    connector.getState() = GameSessionController::State::ON_LOBBY;
+                    connector.getController().getlobby()->Start();
+                }
+                break;
+            }
+            default:
+                break;
+        }
+        connector.pollEvent();
+    }
 }
 
 void BomberWave::Launch()
